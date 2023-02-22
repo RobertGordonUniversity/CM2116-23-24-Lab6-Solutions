908ba1b6ae70132d3a75569814c1b6691c155a99
Mark Snaith <m.snaith@rgu.ac.uk>
2023-02-22 13:46:04 +0000

    Implementing solutions

--- src/main/java/ac/rgu/cm2116/ArrayList.java
+++ src/main/java/ac/rgu/cm2116/ArrayList.java
@@ -3,43 +3,107 @@ package ac.rgu.cm2116;
 public class ArrayList<T> extends SoCList<T> {
 
     private Object[] array;
+    private int size; // added size field because efficiency update means we can't rely on the array size
 
-    public ArrayList(){
-        this.array = new Object[0];
+    public ArrayList() {
+        this.array = new Object[10]; // revised, more efficient version that gives the array an initial size > 0
+        this.size = 0;
     }
 
     @Override
-    public boolean add(T e){
-        return false;
-    }
+    public boolean add(T e) {
 
-    @Override
-    public T remove(int index){
-        return null;
-    }
+        /*
+         * Do we need to resize our array? Yes, if this.size is the length of the array
+         */
+        if (this.size == array.length) {
+            Object[] temp = new Object[array.length + 10];
 
-    @Override
-    public boolean remove(Object e){
-        return false;
-    }
+            for (int i = 0; i < this.array.length; i++) {
+                temp[i] = this.array[i];
+            }
 
-    @Override
-    public T get(int index){
-        return null;
-    }
+            this.array = temp;
+        }
 
-    @Override
-    public int size(){
-        return 0;
-    }
+        /* Add the item and increment size */
+        this.array[this.size] = e;
+        this.size++;
 
-    @Override
-    public boolean isEmpty(){
         return true;
     }
 
     @Override
-    public void clear(){
-        
-    }   
+    public T remove(int index) {
+
+        T value = null;
+
+        /* Index must be less than size - otherwise, it's out of bounds */
+        if (index < size) {
+            value = (T) this.array[index];
+
+            /* Create a new array that's the size of the current array - remember, the size might be bigger than the number of elements being stored */
+            Object[] temp = new Object[this.array.length];
+
+            /* Copy over all elements before index */
+            for (int i = 0; i < index; i++) {
+                temp[i] = this.array[i];
+            }
+
+            /* Copy over all elements after index */
+            for (int i = index + 1; i < this.size; i++) {
+                temp[i - 1] = this.array[i];
+            }
+
+            /* Reduce the size */
+            this.size--;
+        }
+
+        return value;
+    }
+
+    @Override
+    public boolean remove(Object e) {
+        /* Search for the item in the list; if it exists, use the remove(int index) method to actually remove it */
+        for (int i = 0; i < this.size; i++) {
+            if (e.equals(this.array[i])) {
+                if (this.remove(i) != null) {
+                    return true;
+                }
+            }
+        }
+
+        /* If we get this far, we can only return false */
+        return false;
+
+    }
+
+    @Override
+    public T get(int index) {
+        T value = null;
+        /* Index must be less than size */
+        if (index < this.size) {
+            value = (T) this.array[index];
+        }
+
+        return value;
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        /* An empty list has a size of 0 */
+        return (this.size == 0);
+    }
+
+    @Override
+    public void clear() {
+        /* Clearing is easy - new empty array and size=0 */
+        this.array = new Object[10];
+        this.size = 0;
+    }
 }
--- src/main/java/ac/rgu/cm2116/LinkedList.java
+++ src/main/java/ac/rgu/cm2116/LinkedList.java
@@ -5,44 +5,131 @@ public class LinkedList<T> extends SoCList<T> {
     private ListNode<T> head;
     private int size;
 
-    public LinkedList(){
+    public LinkedList() {
         this.head = null;
         this.size = 0;
     }
 
     @Override
-    public boolean add(T e){
-        return false;
-    }
+    public boolean add(T e) {
+        /* ListNode to store the value */
+        ListNode<T> newNode = new ListNode<>(e);
 
-    @Override
-    public T remove(int index){
-        return null;
-    }
+        /* If we've no head...we do now */
+        if (this.head == null) {
+            this.head = newNode;
+        } else {
+            /* Otherwise, iterate until we find the last ListNode and attach the new one to it */
+            ListNode<T> temp = this.head;
 
-    @Override
-    public boolean remove(Object e){
-        return false;
-    }
+            while (temp.getNext() != null) {
+                temp = temp.getNext();
+            }
 
-    @Override
-    public T get(int index){
-        return null;
-    }
-
-    @Override
-    public int size(){
-        return 0;
-    }
-
-    @Override
-    public boolean isEmpty(){
+            temp.setNext(newNode);
+        }
+        this.size++;
         return true;
     }
 
     @Override
-    public void clear(){
-        
+    public T remove(int index) {
+
+        T value = null;
+
+        /* We can only remove if the index is less than the size of the list */
+        if (index < this.size) {
+
+            /* An index of 0 is the head */
+            if (index == 0) {
+                value = this.head.getValue();
+                /* We make the new head the old head's next */
+                this.head = this.head.getNext();
+            } else {
+                /* Otherwise, we loop through each node, incrementing a counter until we reach the node ONE BEFORE the one we want */
+                ListNode<T> temp = this.head;
+
+                int counter = 0;
+
+                while (counter < index - 1) {
+                    temp = temp.getNext();
+                    counter++;
+                }
+
+                /* We get the value from the NEXT node, then make the current node's next the next node's next */
+                value = temp.getNext().getValue();
+                temp.setNext(temp.getNext().getNext());
+            }
+            /* Remember to reduce the size of the list */
+            this.size--;
+        }
+        return value;
+    }
+
+    @Override
+    public boolean remove(Object e) {
+        ListNode<T> temp = this.head;
+        int counter = 0;
+
+        /* Loop over all nodes until we find the value we want, if it exists */
+        while(temp != null){
+            /* If the value does exist, use the remote(int index) method to actually remove it */
+            if(temp.getValue().equals(e)){
+                if(this.remove(counter) != null){
+                    return true;
+                }
+            }
+            counter++;
+        }
+        return false;
+    }
+
+    @Override
+    public T get(int index) {
+        T value = null;
+
+        /* We can only get an index less than the size of the list */
+        if (index < this.size) {
+
+            /* If the index is 0, it's the value stored in head */
+            if (index == 0) {
+                value = this.head.getValue();
+            } else {
+
+                /* Otherwise, iterate until our counter reaches the index */
+                ListNode<T> temp = this.head;
+
+                int counter = 0;
+
+                while (counter < index) {
+                    temp = temp.getNext();
+                    counter++;
+                }
+                value = temp.getValue();
+            }
+
+        }
+        return value;
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        /* An empty list has a sie of 0 */
+        return (this.size == 0);
+    }
+
+    @Override
+    public void clear() {
+        /* Setting head=null and size=0 is enough to clear a list 
+         * In other languages without automatic garbage collection (e.g. C, C++) you'd need to delete each node individually
+        */
+        this.head = null;
+        this.size = 0;
     }
 
     /** Don't edit below this line - here be dragons :) */
@@ -51,27 +138,27 @@ public class LinkedList<T> extends SoCList<T> {
      * Inner class to represent a List Node
      * S is used as the type parameter to avoid conflict with T in the outer class
      */
-    class ListNode<S>{
+    class ListNode<S> {
 
         private S value;
         private ListNode<S> next;
 
-        public ListNode(S value){
+        public ListNode(S value) {
             this.value = value;
             this.next = null;
         }
 
-        public ListNode<S> getNext(){
+        public ListNode<S> getNext() {
             return this.next;
         }
 
-        public void setNext(ListNode<S> next){
+        public void setNext(ListNode<S> next) {
             this.next = next;
         }
 
-        public S getValue(){
+        public S getValue() {
             return this.value;
         }
     }
-    
+
 }
